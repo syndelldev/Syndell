3b5db7758c8ee6840e7cb8cfeeb3750bc3ab875b
riya patel <riya.syndell@gmail.com>
2022-11-15 18:56:16 +0530

    Slider added

--- src/home.js
+++ src/home.js
@@ -1,61 +1,74 @@
 import React, { useEffect, useState } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './App.css';
 
 function MyComponent() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch("https://syndelltech.com/wp-json/acf/v3/home-reactjs")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-
-
-  var data = items[0];
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-
-    var slider = data.acf.slider;
-
-    return (
-      <>
-
-        <div className="slider-section">
-            {(slider).map(s_data => (
-              <div className="slider_content">
-              {console.log(s_data)}
-
-                <div className='slider_text'>
-                  <h2 className='slider_title'> {s_data.slider_title} </h2>
-                  <a href='/contact-us' className='slider_button'>Get a Proposal</a>
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    useEffect(() => {
+      fetch("https://syndelltech.com/wp-json/acf/v3/home-reactjs")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+    
+    var data = items[0];
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+  
+      var slider = data.acf.slider;
+  
+      return (
+        <>
+  <div className="container">
+          <div className="slider-section">
+          <Slider {...settings}>
+              {(slider).map(s_data => (
+                <div className="slider_content">
+                {console.log(s_data)}
+  
+                  <div className='slider_text'>
+                    <h2 className='slider_title'> {s_data.slider_title} </h2>
+                    <a href='/contact-us' className='slider_button'>Get a Proposal</a>
+                  </div>
+  
+                  <div className='slider_img'>
+                    <img src={s_data.slider_image.url} alt="Slider" />
+                  </div>
+  
                 </div>
+              ))}
+          </Slider>
+          </div>
 
-                <div className='slider_img'>
-                  <img src={s_data.slider_image.url} alt="Slider" />
-                </div>
-
-              </div>
-            ))}
-        </div>
-
-      </>
-    );
+          </div>  
+        </>
+      );
+    }
   }
-}
-
+  
 export default MyComponent;
